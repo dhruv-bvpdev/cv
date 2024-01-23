5c554b761d4f7ad4bf0601d2a49e42cf8c5445fd
Dhruv Gursahani <dhruv.gursahani@outlook.com>
2024-01-23 14:56:55 +0530

    fix: :construction_worker: added favicon and location link

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import { Badge } from '../components/ui/badge'
 import { CommandMenu } from '../components/command-menu'
 import { Metadata } from 'next'
 import { Section } from '../components/ui/section'
-import { MailIcon, PhoneIcon } from 'lucide-react'
+import { GlobeIcon, MailIcon, PhoneIcon } from 'lucide-react'
 import { Button } from '../components/ui/button'
 import { RESUME_DATA } from '../data/resume-data'
 import { ProjectCard } from '../components/project-card'
@@ -17,15 +17,22 @@ export const metadata: Metadata = {
 export default function Page() {
   return (
     <main className="container relative mx-auto scroll-my-12 overflow-auto p-4 md:p-16 print:p-12">
-      <section className="mx-auto w-full max-w-2xl space-y-8 bg-white">
+      <section className="mx-auto w-full max-w-2xl space-y-8 bg-white print:space-y-6">
         <div className="flex items-center justify-between">
-          <div className="flex-1 space-y-1">
+          <div className="flex-1 space-y-1.5">
             <h1 className="text-2xl font-bold">{RESUME_DATA.name}</h1>
             <p className="max-w-md text-pretty font-mono text-sm text-muted-foreground">
               {RESUME_DATA.about}
             </p>
-            <p className="max-w-md text-pretty font-mono text-xs text-muted-foreground">
-              {RESUME_DATA.location}
+            <p className="max-w-md items-center text-pretty font-mono text-xs text-muted-foreground">
+              <a
+                className="inline-flex gap-x-1.5 align-baseline leading-none hover:underline"
+                href={RESUME_DATA.locationLink}
+                target="_blank"
+              >
+                <GlobeIcon className="h-3 w-3" />
+                {RESUME_DATA.location}
+              </a>
             </p>
             <div className="flex gap-x-1 pt-1 font-mono text-sm text-muted-foreground print:hidden">
               {RESUME_DATA.contact.email ? (
