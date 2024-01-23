7237127cf2460bb9b903a4284539ef2b41dd53ad
Dhruv Gursahani <dhruv.gursahani@outlook.com>
2024-01-23 13:27:22 +0530

    chore: :zap: metadata, code cleanup, scroll margin

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,13 +4,15 @@ import { Avatar, AvatarFallback, AvatarImage } from '../components/ui/avatar'
 import { Card, CardHeader, CardContent } from '../components/ui/card'
 import { Badge } from '../components/ui/badge'
 import { CommandMenu } from '../components/command-menu'
+import { Metadata } from 'next'
+import { Section } from '../components/ui/section'
 
-const ResumeData = {
+const RESUME_DATA = {
   name: 'Dhruv Gursahani',
   initials: 'DG',
   about: 'MBA Student',
   summary:
-    ' Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.',
+    'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.',
   avatarUrl: '',
   personalWebsiteUrl: 'https://dhruvgursahani.vercel.app',
   contact: {
@@ -60,6 +62,8 @@ const ResumeData = {
   work: [
     {
       company: 'Corizo',
+      link: 'https://corizo.in',
+      badges: ['Bangalore'],
       title: 'Summer Intern',
       logo: Corizo,
       start: 'May 2023',
@@ -68,6 +72,8 @@ const ResumeData = {
     },
     {
       company: 'Self Employed',
+      link: '',
+      badges: ['Remote'],
       title: 'Free-Lance Web Developer',
       logo: selfEmp,
       start: '2021',
@@ -81,110 +87,128 @@ const ResumeData = {
     'React/Next.js/Remix',
     'Node.js',
     'GraphQL',
+    'Relay',
     'WebRTC'
   ]
 } as const
 
-const getResumeData = async () => {
-  return ResumeData
+export const metadata: Metadata = {
+  title: `${RESUME_DATA.name} | ${RESUME_DATA.about}`,
+  description: RESUME_DATA.summary
 }
 
-export default async function Home() {
-  const resumeData = await getResumeData()
-
+export default function Page() {
   return (
-    <main className="container mx-auto p-1 md:p-6 relative overflow-auto">
-      <section className="w-full max-w-2xl mx-auto bg-white p-6 space-y-6">
-        <div className="flex items-center justify-between mb-6">
+    <main className="container mx-auto p-1 md:p-12 relative overflow-auto scroll-mb-12">
+      <section className="w-full max-w-2xl mx-auto bg-white space-y-8">
+        <div className="flex items-center justify-between">
           <div className="flex-1 space-y-1">
-            <h1 className="text-2xl font-bold">{resumeData.name}</h1>
-            <p className="text-muted-foreground font-mono text-base max-w-md text-pretty">
-              {resumeData.about}
+            <h1 className="text-2xl font-bold">{RESUME_DATA.name}</h1>
+            <p className="text-muted-foreground font-mono text-sm max-w-md text-pretty">
+              {RESUME_DATA.about}
             </p>
+            <div className="text-muted-foreground font-mono flex flex-col text-sm gap-x-1">
+              {RESUME_DATA.contact.email ? (
+                <a href={`mailto:${RESUME_DATA.contact.email}`}>
+                  <span className="underline">{RESUME_DATA.contact.email}</span>
+                </a>
+              ) : null}
+              {RESUME_DATA.contact.tel ? (
+                <a href={`tel:${RESUME_DATA.contact.tel}`}>
+                  <span className="underline">{RESUME_DATA.contact.tel}</span>
+                </a>
+              ) : null}
+            </div>
           </div>
 
           <Avatar className="h-28 w-28">
-            <AvatarImage alt={resumeData.name} src={resumeData.avatarUrl} />
-            <AvatarFallback>{resumeData.initials}</AvatarFallback>
+            <AvatarImage alt={RESUME_DATA.name} src={RESUME_DATA.avatarUrl} />
+            <AvatarFallback>{RESUME_DATA.initials}</AvatarFallback>
           </Avatar>
         </div>
-        <section className="space-y-1">
+        <Section>
           <h2 className="text-xl font-bold">Summary</h2>
           <p className="text-muted-foreground font-mono text-sm text-pretty">
-            {resumeData.summary}
+            {RESUME_DATA.summary}
           </p>
-        </section>
-        <section className="flex flex-col min-h-0 gap-y-5">
-          <h2 className="text-xl font-bold my-2">Work Experience</h2>
-          {resumeData.work.map(work => {
+        </Section>
+        <Section>
+          <h2 className="text-xl font-bold">Work Experience</h2>
+          {RESUME_DATA.work.map(work => {
             return (
               <Card key={work.company}>
                 <CardHeader>
-                  <div className="flex items-center justify-between gap-x-2 text-lg">
-                    <h3 className="font-semibold leading-none">
-                      {work.company}
+                  <div className="flex items-center justify-between gap-x-2 text-base">
+                    <h3 className="font-semibold leading-none inline-flex items-center justify-center gap-x-1">
+                      <a className="hover:underline" href={work.link}>
+                        {work.company}
+                      </a>
+
+                      <span className="inline-flex gap-x-1">
+                        {work.badges.map(badge => (
+                          <Badge
+                            variant="secondary"
+                            className="text-xs align-middle"
+                            key={badge}
+                          >
+                            {badge}
+                          </Badge>
+                        ))}
+                      </span>
                     </h3>
-                    <div className="text-gray-500 text-sm">
+                    <div className="text-gray-500 text-sm tabular-nums">
                       {work.start} - {work.end}
                     </div>
                   </div>
 
-                  <h4 className="text-sm leading-none">{work.title}</h4>
+                  <h4 className="text-sm leading-none font-mono">
+                    {work.title}
+                  </h4>
                 </CardHeader>
-                <CardContent className="mt-2">{work.description}</CardContent>
+                <CardContent className="mt-2 text-xs">
+                  {work.description}
+                </CardContent>
               </Card>
             )
           })}
-        </section>
-        <section>
-          <h2 className="text-xl font-bold mb-4">Education</h2>
-          {resumeData.education.map(education => {
+        </Section>
+        <Section>
+          <h2 className="text-xl font-bold">Education</h2>
+          {RESUME_DATA.education.map(education => {
             return (
               <Card key={education.school}>
                 <CardHeader>
-                  <h3 className="text-lg font-semibold">{education.school}</h3>
-                  <p className="text-gray-500">
-                    {education.degree}, {education.start} - {education.end}
-                  </p>
+                  <div className="flex items-center justify-between gap-x-2 text-base">
+                    <h3 className="font-semibold leading-none">
+                      {education.school}
+                    </h3>
+                    <div className="text-gray-500 text-sm tabular-nums">
+                      {education.start} - {education.end}
+                    </div>
+                  </div>
                 </CardHeader>
+                <CardContent className="mt-2">{education.degree}</CardContent>
               </Card>
             )
           })}
-        </section>
-        <section>
-          <h2 className="text-xl font-bold mb-4">Skills</h2>
-          <div className="flex flex-wrap gap-2">
-            {resumeData.skills.map(skill => {
+        </Section>
+        <Section>
+          <h2 className="text-xl font-bold">Skills</h2>
+          <div className="flex flex-wrap gap-1">
+            {RESUME_DATA.skills.map(skill => {
               return <Badge key={skill}>{skill}</Badge>
             })}
           </div>
-        </section>
-        <section>
-          <h2 className="text-xl font-bold mb-4">Contact</h2>
-          <div className="flex flex-col gap-2 text-sm">
-            {resumeData.contact.email ? (
-              <a href={`mailto:${resumeData.contact.email}`}>
-                Mail:{' '}
-                <span className="underline">{resumeData.contact.email}</span>
-              </a>
-            ) : null}
-            {resumeData.contact.tel ? (
-              <a href={`tel:${resumeData.contact.tel}`}>
-                Mobile:{' '}
-                <span className="underline">{resumeData.contact.tel}</span>
-              </a>
-            ) : null}
-          </div>
-        </section>
+        </Section>
       </section>
 
       <CommandMenu
         links={[
           {
-            url: resumeData.personalWebsiteUrl,
+            url: RESUME_DATA.personalWebsiteUrl,
             title: 'Personal Website'
           },
-          ...resumeData.contact.social.map(socilaMediaLink => ({
+          ...RESUME_DATA.contact.social.map(socilaMediaLink => ({
             url: socilaMediaLink.url,
             title: socilaMediaLink.name
           }))
